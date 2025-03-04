0bbfc24b6cbb695123d8294c77ae041884d5a471
Emilio Rodriguez Larracilla <emiliorodriguezlarracilla@CLAP1907-1442.local>
2025-03-03 22:15:19 -0600

    creacion de login

---
+++ clase-4-3marzo/ejercicio_CV_estilos/index.js
@@ -0,0 +1,37 @@
+console.log("ejercicio de CV con login");
+
+const d = document;
+const modal = d.querySelector(".modal");
+modal.addEventListener("click", () => {
+  modal.close();
+  form.reset();
+});
+
+const userAccess = {
+  user: "emilio",
+  password: "lamejordetodas45",
+};
+let userValues = {};
+
+const form = d.querySelector(".formLogin");
+console.log(form);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  userValues = {
+    user: form.user.value,
+    password: form.password.value,
+  };
+
+  console.log(userValues);
+
+  if (
+    userAccess.user === userValues.user &&
+    userAccess.password === userValues.password
+  ) {
+    window.location.href = "/CV_emilio.html";
+  } else {
+    modal.showModal();
+  }
+});
